ad6b1cb310c7ed6878ca301b9e5ac1ed3a8e6247
TakuKitamura <takukitamura.io@gmail.com>
2021-12-11 22:11:49 +0900

    add tables on terminal user interface

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,20 @@
 use clap::{crate_authors, crate_description, crate_version};
-use clap::{App, Arg, SubCommand};
+use clap::{Arg, SubCommand};
 use dirs;
+
+use crossterm::{
+    event::{self, DisableMouseCapture, EnableMouseCapture, Event, KeyCode},
+    execute,
+    terminal::{disable_raw_mode, enable_raw_mode, EnterAlternateScreen, LeaveAlternateScreen},
+};
+use std::io;
+use tui::{
+    backend::{Backend, TermionBackend},
+    layout::{Constraint, Layout},
+    style::{Color, Modifier, Style},
+    widgets::{Block, Borders, Cell, Row, Table, TableState},
+    Frame, Terminal,
+};
 
 // get history vector with duplicates removed
 fn get_tidy_history() -> Result<Vec<String>, std::io::Error> {
@@ -31,8 +45,127 @@ fn get_tidy_history() -> Result<Vec<String>, std::io::Error> {
     };
 }
 
+struct App<'a> {
+    state: TableState,
+    items: Vec<Vec<&'a str>>,
+}
+
+impl<'a> App<'a> {
+    fn new() -> App<'a> {
+        App {
+            state: TableState::default(),
+            items: vec![
+                vec!["Row11"],
+                vec!["Row21"],
+                vec!["Row31"],
+                vec!["Row41"],
+                vec!["Row51"],
+                vec!["Row61"],
+                vec!["Row71"],
+                vec!["Row81"],
+                vec!["Row91"],
+                vec!["Row101"],
+                vec!["Row111"],
+                vec!["Row121"],
+                vec!["Row131"],
+                vec!["Row141"],
+                vec!["Row151"],
+                vec!["Row161"],
+                vec!["Row171"],
+                vec!["Row181"],
+                vec!["Row191"],
+            ],
+        }
+    }
+    pub fn next(&mut self) {
+        let i = match self.state.selected() {
+            Some(i) => {
+                if i >= self.items.len() - 1 {
+                    0
+                } else {
+                    i + 1
+                }
+            }
+            None => 0,
+        };
+        self.state.select(Some(i));
+    }
+
+    pub fn previous(&mut self) {
+        let i = match self.state.selected() {
+            Some(i) => {
+                if i == 0 {
+                    self.items.len() - 1
+                } else {
+                    i - 1
+                }
+            }
+            None => 0,
+        };
+        self.state.select(Some(i));
+    }
+}
+
+fn run_app<B: Backend>(terminal: &mut Terminal<B>, mut app: App) -> io::Result<()> {
+    loop {
+        terminal.draw(|f| ui(f, &mut app))?;
+
+        if let Event::Key(key) = event::read()? {
+            match key.code {
+                KeyCode::Char('q') => return Ok(()),
+                KeyCode::Down => app.next(),
+                KeyCode::Up => app.previous(),
+                _ => {}
+            }
+        }
+    }
+}
+
+fn ui<B: Backend>(f: &mut Frame<B>, app: &mut App) {
+    let rects = Layout::default()
+        .horizontal_margin(3)
+        .vertical_margin(3)
+        .constraints([Constraint::Percentage(100)].as_ref())
+        .split(f.size());
+
+    let selected_style = Style::default().add_modifier(Modifier::REVERSED);
+    let normal_style = Style::default().bg(Color::Blue);
+    let header_cells = ["Tags"]
+        .iter()
+        .map(|h| Cell::from(*h).style(Style::default().fg(Color::Red)));
+    let header = Row::new(header_cells)
+        .style(normal_style)
+        .height(1)
+        .bottom_margin(1);
+    let rows = app.items.iter().map(|item| {
+        let height = item
+            .iter()
+            .map(|content| content.chars().filter(|c| *c == '\n').count())
+            .max()
+            .unwrap_or(0)
+            + 1;
+        let cells = item.iter().map(|c| Cell::from(*c));
+        Row::new(cells).height(height as u16)
+    });
+    let t = Table::new(rows)
+        .header(header)
+        .block(
+            Block::default()
+                .borders(Borders::ALL)
+                .title(" History Tags "),
+        )
+        .highlight_style(selected_style)
+        .highlight_symbol("> ")
+        .widths(&[
+            Constraint::Percentage(100),
+            Constraint::Length(30),
+            Constraint::Min(10),
+        ]);
+    f.render_stateful_widget(t, rects[0], &mut app.state);
+}
+
 fn main() {
-    let matches = App::new("history-tidy")
+    let matches = clap::App::new("history-tidy")
         .version(crate_version!())
         .author(crate_authors!())
         .about(crate_description!())
@@ -56,14 +189,38 @@ fn main() {
             _ => unreachable!(),
         };
         println!("{}", init_shell_script);
-       std::process::exit(0);
+        std::process::exit(0);
     }
 
     // no args or subcommand is 'list' case
     match get_tidy_history() {
         Ok(history_vec) => {
-            for line in history_vec {
-                println!("{}", line);
+            // for line in history_vec {
+            //     println!("{}", line);
+            // }
+
+            enable_raw_mode().unwrap();
+            let mut stdout = io::stdout();
+            execute!(stdout, EnterAlternateScreen, EnableMouseCapture).unwrap();
+            let backend = TermionBackend::new(stdout);
+            let mut terminal = Terminal::new(backend).unwrap();
+
+            let mut app = App::new();
+            app.next();
+            let res = run_app(&mut terminal, app);
+
+            // restore terminal
+            disable_raw_mode().unwrap();
+            execute!(
+                terminal.backend_mut(),
+                LeaveAlternateScreen,
+                DisableMouseCapture
+            )
+            .unwrap();
+            terminal.show_cursor().unwrap();
+
+            if let Err(err) = res {
+                eprintln!("{:?}", err)
             }
         }
         Err(e) => {
