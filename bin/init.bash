0a0429c8b6cff34130965852275a3febc7ff62a5
TakuKitamura <takukitamura.io@gmail.com>
2021-12-11 16:03:15 +0900

    add 'history-tidy init' command to append a history line to a .history-tidy file

---
+++ bin/init.bash
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+# set PROMPT_COMMAND to append a history line to a .history-tidy file
+history_tidy_prompt="history -a ~/.history-tidy"
+if [ -z "$PROMPT_COMMAND" ]
+    then
+        # not already set
+        PROMPT_COMMAND=history_tidy_prompt
+    else
+        # already set
+        PROMPT_COMMAND="${history_tidy_prompt};${PROMPT_COMMAND#;}"
+fi
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,28 @@
+use clap::{crate_authors, crate_description, crate_version};
+use clap::{App, Arg, SubCommand};
+
 fn main() {
-    println!("Hello, world!");
+    let matches = App::new("history-tidy")
+        .version(crate_version!())
+        .author(crate_authors!())
+        .about(crate_description!())
+        .subcommand(
+            SubCommand::with_name("init")
+                .about("print the shell script to execute history-tidy")
+                .arg(
+                    Arg::with_name("shell-type")
+                        .possible_values(&["bash"])
+                        .required(true),
+                ),
+        )
+        .get_matches();
+
+    if let Some(matches) = matches.subcommand_matches("init") {
+        let shell = matches.value_of("shell-type").unwrap();
+        let init_shell_script = match shell {
+            "bash" => include_str!("../bin/init.bash"),
+            _ => unreachable!(),
+        };
+        println!("{}", init_shell_script);
+    }
 }
