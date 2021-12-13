41222748110248adf76c269fc6690fc385f23bfa
TakuKitamura <takukitamura.io@gmail.com>
2021-12-13 20:22:07 +0900

    add gui

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,25 @@
+use clap::{crate_authors, crate_description, crate_version};
+use clap::{Arg, SubCommand};
 use conch_parser::lexer::Lexer;
 use conch_parser::parse::DefaultParser;
 use dirs;
 use std::collections::HashMap;
 mod hashtag;
 use hashtag::HashtagParser;
+
+use crossterm::{
+    event::{self, DisableMouseCapture, EnableMouseCapture, Event, KeyCode},
+    execute,
+    terminal::{disable_raw_mode, enable_raw_mode, EnterAlternateScreen, LeaveAlternateScreen},
+};
+use std::io;
+use tui::{
+    backend::{Backend, TermionBackend},
+    layout::{Constraint, Layout},
+    style::{Color, Modifier, Style},
+    widgets::{Block, Borders, Cell, Row, Table, TableState},
+    Frame, Terminal,
+};
 
 // get history vector with duplicates removed
 fn get_tidy_history() -> Result<Vec<String>, std::io::Error> {
@@ -39,10 +55,27 @@ fn get_tidy_history() -> Result<Vec<String>, std::io::Error> {
 // }
 
 fn main() {
+    // let matches = clap::App::new("history-tidy")
+    //     .version(crate_version!())
+    //     .author(crate_authors!())
+    //     .about(crate_description!())
+    //     .subcommand(SubCommand::with_name("list").about("listing history with tags"))
+    //     .subcommand(
+    //         SubCommand::with_name("init")
+    //             .about("print the shell script to execute history-tidy")
+    //             .arg(
+    //                 Arg::with_name("shell-type")
+    //                     .possible_values(&["bash"])
+    //                     .required(true),
+    //             ),
+    //     )
+    //     .get_matches();
+
+    let mut map: HashMap<String, HashMap<String, String>> = HashMap::new();
     match get_tidy_history() {
         Ok(history_vec) => {
             // tag: command: message
-            let mut map: HashMap<String, HashMap<String, String>> = HashMap::new();
+
             for history in &history_vec {
                 let lexer = Lexer::new(history.chars());
                 let mut parser = DefaultParser::new(lexer);
@@ -99,35 +132,32 @@ fn main() {
                 }
             }
 
-            for (tag, map_hashtag) in &map {
-                println!("tag:{}", tag);
-                // for (history, message) in map_hashtag {
-                //     println!("history:{}", history);
-                //     println!("message:{}", message);
-                // }
-            }
+            // println!("{:?}", map);
+            // println!("{:?}", history_vec);
 
-            // for (tag, map_hashtag) in &map {
-            //     let x = map.get_key_value("#abc");
-            for (_, message) in map.get_key_value("#abc") {
-                for (y, z) in message {
-                    // get history index in enmuulate history_vec
-                    let mut i = 1;
-                    for h in &history_vec {
-                        if h.trim() == y.trim() {
-                            println!("#abc:id:{}", i);
-                            break;
-                        }
-                        i += 1;
-                    }
+            enable_raw_mode().unwrap();
+            let mut stdout = io::stdout();
+            execute!(stdout, EnterAlternateScreen, EnableMouseCapture).unwrap();
+            let backend = TermionBackend::new(stdout);
+            let mut terminal = Terminal::new(backend).unwrap();
 
-                    println!("#abc:history:{}", y);
-                    println!("#abc:message:{}", z);
-                }
-                // println!("history:{}", history);
-                // println!("message:{:?}", message);
+            let mut app = App::new(map, history_vec);
+            app.next();
+            let res = run_app(&mut terminal, app);
+
+            // restore terminal
+            disable_raw_mode().unwrap();
+            execute!(
+                terminal.backend_mut(),
+                LeaveAlternateScreen,
+                DisableMouseCapture
+            )
+            .unwrap();
+            terminal.show_cursor().unwrap();
+
+            if let Err(err) = res {
+                eprintln!("{:?}", err)
             }
-            // }
         }
         Err(e) => {
             eprintln!("Failed to load the file: {}", e);
@@ -135,4 +165,111 @@ fn main() {
         }
     }
     std::process::exit(0);
+}
+
+struct App {
+    state: TableState,
+    items: Vec<Vec<String>>,
+}
+
+impl App {
+    fn new(history_map: HashMap<String, HashMap<String, String>>, history: Vec<String>) -> App {
+        let mut hashtags = vec![];
+        for hashtag in (&history_map).keys() {
+            let item_count = history_map.get(hashtag).unwrap().len();
+            hashtags.push(vec![hashtag.to_string(), item_count.to_string()]);
+        }
+
+        // println!("{:?}", hashtags);
+
+        App {
+            state: TableState::default(),
+            items: hashtags,
+        }
+    }
+    pub fn next(&mut self) {
+        let i = match self.state.selected() {
+            Some(i) => {
+                if i >= self.items.len() - 1 {
+                    0
+                } else {
+                    i + 1
+                }
+            }
+            None => 0,
+        };
+        self.state.select(Some(i));
+    }
+
+    pub fn previous(&mut self) {
+        let i = match self.state.selected() {
+            Some(i) => {
+                if i == 0 {
+                    self.items.len() - 1
+                } else {
+                    i - 1
+                }
+            }
+            None => 0,
+        };
+        self.state.select(Some(i));
+    }
+}
+
+fn run_app<B: Backend>(terminal: &mut Terminal<B>, mut app: App) -> io::Result<()> {
+    loop {
+        terminal.draw(|f| ui(f, &mut app))?;
+
+        if let Event::Key(key) = event::read()? {
+            match key.code {
+                KeyCode::Char('q') => return Ok(()),
+                KeyCode::Down => app.next(),
+                KeyCode::Up => app.previous(),
+                _ => {}
+            }
+        }
+    }
+}
+
+fn ui<B: Backend>(f: &mut Frame<B>, app: &mut App) {
+    let rects = Layout::default()
+        .horizontal_margin(3)
+        .vertical_margin(3)
+        .constraints([Constraint::Percentage(100)].as_ref())
+        .split(f.size());
+
+    let selected_style = Style::default().add_modifier(Modifier::REVERSED);
+    let normal_style = Style::default().bg(Color::Blue);
+    let header_cells = ["Tags", "Item"]
+        .iter()
+        .map(|h| Cell::from(*h).style(Style::default().fg(Color::Red)));
+    let header = Row::new(header_cells)
+        .style(normal_style)
+        .height(1)
+        .bottom_margin(1);
+    let rows = app.items.iter().map(|item| {
+        let height = item
+            .iter()
+            .map(|content| content.chars().filter(|c| *c == '\n').count())
+            .max()
+            .unwrap_or(0)
+            + 1;
+        let cells = item.iter().map(|content| content.to_string());
+        Row::new(cells).height(height as u16)
+    });
+    let t = Table::new(rows)
+        .header(header)
+        .block(
+            Block::default()
+                .borders(Borders::ALL)
+                .title(" History Tags "),
+        )
+        .highlight_style(selected_style)
+        .highlight_symbol("> ")
+        .widths(&[
+            Constraint::Percentage(50),
+            Constraint::Length(30),
+            Constraint::Min(10),
+        ]);
+    f.render_stateful_widget(t, rects[0], &mut app.state);
 }
