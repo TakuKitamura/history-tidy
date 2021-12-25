4df74474183a482b59ca12c009363417601eb1ce
TakuKitamura <takukitamura.io@gmail.com>
2021-12-25 12:52:09 +0900

    fix ui

--- src/setting.rs
+++ src/setting.rs
@@ -1,31 +1,56 @@
-use clap::{crate_authors, crate_description, crate_version};
-use clap::{App, Arg, ArgMatches, SubCommand};
+use colored::*;
+use std::env;
 use std::process::exit;
 
-pub fn command_line_setting() {
-    let matches: ArgMatches = App::new("history-tidy")
-        .version(crate_version!())
-        .author(crate_authors!())
-        .about(crate_description!())
-        .subcommand(SubCommand::with_name("load").about("load command selected"))
-        .subcommand(
-            SubCommand::with_name("init")
-                .about("print the shell script to execute history-tidy")
-                .arg(
-                    Arg::with_name("shell-type")
-                        .possible_values(&["bash"])
-                        .required(true),
-                ),
-        )
-        .get_matches();
+const PACKAGE_NAME: &str = env!("CARGO_PKG_NAME");
+const PACKAGE_VERSION: &str = env!("CARGO_PKG_VERSION");
+const PACKAGE_DESCRIPTION: &str = env!("CARGO_PKG_DESCRIPTION");
 
-    if let Some(matches) = matches.subcommand_matches("init") {
-        let shell: &str = matches.value_of("shell-type").unwrap();
-        let init_shell_script: &str = match shell {
-            "bash" => include_str!("../bin/init.bash"),
-            _ => unreachable!(),
-        };
-        println!("{}", init_shell_script);
-        exit(0);
+fn gen_help_string() -> String {
+    return format!(
+        "{package_name} {package_version}
+{package_description}
+
+{usage}:
+    {package_name} [OPTIONS]
+
+{options}:
+    {help}      Prints help information
+    {version}   Prints version information
+    {init_bash}      Initialize bash history file",
+        package_name = PACKAGE_NAME,
+        package_version = PACKAGE_VERSION,
+        package_description = PACKAGE_DESCRIPTION,
+        usage = "USAGE".cyan().bold(),
+        options = "OPTIONS".cyan().bold(),
+        help = "-h, --help".green(),
+        version = "-V, --version".green(),
+        init_bash = "-init-bash".green()
+    );
+}
+
+pub fn command_line_setting() {
+    let args: Vec<String> = env::args().collect();
+
+    if args.len() >= 2 {
+        let option: &String = &args[1];
+        if option == "-h" || option == "--help" {
+            println!("{}", gen_help_string());
+            exit(0);
+        } else if option == "-V" || option == "--version" {
+            println!("{} {}", PACKAGE_NAME, PACKAGE_VERSION);
+            exit(0);
+        } else if option == "-init-bash" {
+            println!("{}", include_str!("../bin/init.bash"));
+            exit(0);
+        } else {
+            eprintln!(
+                "{}: Unknown argument '{}'\n",
+                "error".red().bold(),
+                option.yellow()
+            );
+            println!("{}", gen_help_string());
+            exit(1);
+        }
     }
 }
